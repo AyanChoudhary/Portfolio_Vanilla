aab519304d7cb6261d785cee1fb3e6fb31cab0f1
Ayan Choudhary <youremail@domain.com>
2019-02-07 23:59:18 +0530

    test for AJAX

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function openclose()
 {
 	if (x%2 === 0) 
 	{ 
-		document.getElementById('topnav').style.height = "15vh";
+		document.getElementById('topnav').style.height = "20vh";
 		x++; 
 	}
 	
@@ -21,4 +21,34 @@ function closenav()
 		document.getElementById('topnav').style.height = "0vh";
 		x++;
 	}
+}
+
+let path = "/home/ayan/Desktop/Portfolio/"
+
+function switch_About_Me(url)
+{
+	let xhttp = new XMLHttpRequest();
+	xhttp.open("GET", path + url, true);
+	xhttp.responseType = 'text';
+	xhttp.onreadystatechange = function() {
+		if (this.readystate === 4 && this.status === 200) {
+			document.getElementById("parent").innerHTML = this.responseText;
+			document.getElementById("portf").style.fontWeight = "900";
+		}
+	};
+	xhttp.send(null);
+}
+
+function switch_Projects(url)
+{
+	let xhttp = new XMLHttpRequest();
+	xhttp.open("GET", path + url, true);
+	xhttp.responseType = 'text';
+	xhttp.onreadystatechange = function() {
+		if (this.readystate === 4 && this.status === 200) {
+			document.getElementById("parent").innerHTML = this.responseText;
+			document.getElementById("proj").style.fontWeight = "900";
+		}
+	};
+	xhttp.send(null);
 }
